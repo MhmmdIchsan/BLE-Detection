cec83098a2a289387649d64eff2338466eb9ec4a
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-09-21 01:50:12 +0700

    feat: create retrofitinstance

---
+++ app/src/main/java/com/project/blebeacon/RetrofitInstance.kt
@@ -0,0 +1,28 @@
+package com.project.blebeacon
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://api.example.com/"
+
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}

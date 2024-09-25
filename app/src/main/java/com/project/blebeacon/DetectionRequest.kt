08bbe7deb69cb2f1367938a0b6bd4752f0cd3bf8
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-09-25 20:24:29 +0700

    feat: update DetectionRequest data class

--- app/src/main/java/com/project/blebeacon/DetectionRequest.kt
+++ app/src/main/java/com/project/blebeacon/DetectionRequest.kt
@@ -1,7 +1,9 @@
 package com.project.blebeacon
 
 data class DetectionRequest(
+    val deviceid: String,
     val timestamp: String,
     val device: Int,
-    val addresses: List<String>
+    val addresses: List<String>,
+    val rssi: List<Int>
 )

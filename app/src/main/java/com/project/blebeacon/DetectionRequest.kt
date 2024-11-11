58dd7573bac6f61997b57441c411aac39dd70d8e
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-11-11 10:37:13 +0700

    chore: update nullable types

--- app/src/main/java/com/project/blebeacon/DetectionRequest.kt
+++ app/src/main/java/com/project/blebeacon/DetectionRequest.kt
@@ -3,7 +3,7 @@ package com.project.blebeacon
 data class DetectionRequest(
     val deviceid: String,
     val timestamp: String,
-    val device: Int,
-    val addresses: List<String>,
-    val rssi: List<Int>
+    val device: Int = 0,
+    val addresses: List<String>? = null,
+    val rssi: List<Int>? = null
 )

977de9dcab5288b26fdaae7ace6a8ff010a850a9
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-11-13 09:54:12 +0700

    chore: add new API method

--- app/src/main/java/com/project/blebeacon/ApiService.kt
+++ app/src/main/java/com/project/blebeacon/ApiService.kt
@@ -2,9 +2,14 @@ package com.project.blebeacon
 
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiService {
     @POST("bledemo/devices")
     suspend fun postDetection(@Body detectionRequest: DetectionRequest): Response<Unit>
+
+    @GET("bledemo/locationinfo/{deviceId}")
+    suspend fun getLocationInfo(@Path("deviceId") deviceId: String): Response<LocationInfo>
 }

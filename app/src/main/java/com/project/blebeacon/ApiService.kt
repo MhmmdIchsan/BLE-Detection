9db0f57cadcca4276d3b5b3d7c31029618e19c50
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-11-17 01:47:31 +0700

    chore: add locationinfo endpoint to get configuration

--- app/src/main/java/com/project/blebeacon/ApiService.kt
+++ app/src/main/java/com/project/blebeacon/ApiService.kt
@@ -5,11 +5,12 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
     @POST("bledemo/devices")
     suspend fun postDetection(@Body detectionRequest: DetectionRequest): Response<Unit>
 
-    @GET("bledemo/locationinfo/{deviceId}")
-    suspend fun getLocationInfo(@Path("deviceId") deviceId: String): Response<LocationInfo>
+    @GET("bledemo/locationinfo") // Ganti dengan endpoint yang sesuai
+    suspend fun getConfiguration(@Query("deviceid") deviceid: String): Response<ConfigurationResponse>
 }
